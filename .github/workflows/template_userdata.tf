4f2959e7efd948bb6b6206e40d05e17bcf6d1b2d
George Nalen <georgen@mindpointgroup.com>
2022-02-22 15:33:09 -0500

    updated template userdata file
    Signed-off-by: George Nalen <georgen@mindpointgroup.com>

--- .github/workflows/template_userdata.tf
+++ .github/workflows/template_userdata.tf
@@ -1,11 +1,11 @@
 data "template_file" "template_userdata" {
   vars = {
-    new_admin_pass  = "${var.NEW_ADMIN_PASSWORD}"
+    new_admin_pass  = var.NEW_ADMIN_PASSWORD
   }
   template = <<EOF
 <powershell>
 $admin = [adsi]("WinNT://./administrator, user")
-$admin.PSBase.Invoke("SetPassword", "${new_admin_pass}")
+$admin.PSBase.Invoke("SetPassword", "$${new_admin_pass}")
 </powershell>
 EOF
 }
