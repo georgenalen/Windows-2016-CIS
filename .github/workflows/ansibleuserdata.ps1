7f38cc224311dac14c41320e3b3a9096cfd51a23
uk-bolly <69214557+uk-bolly@users.noreply.github.com>
2022-02-22 12:52:18 +0000

    Update ansibleuserdata.ps1

--- .github/workflows/ansibleuserdata.ps1
+++ .github/workflows/ansibleuserdata.ps1
@@ -1,12 +1,4 @@
 ＜powershell＞
-  import-Module AWSPowershell
-  $SecretAD = "WINDOWS_PW"
-  $SecretObj = (Get-SECSecretValue -SecretId $SecretAD)
-  $Secret = ($SecretObj.SecretString  | ConvertFrom-Json)
-  $password   = $Secret.Password | ConvertTo-SecureString -asPlainText -Force
-
-  $UserAccount = Get-LocalUser -Name "administrator"
-  $UserAccount | Set-LocalUser -Password $Password
 
   [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls, [Net.SecurityProtocolType]::Tls11, [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Ssl3
   [Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
